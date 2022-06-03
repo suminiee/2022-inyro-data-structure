a21fad8951f43ca681c3b65ff08b9aa086051f2e
suminiee <90146604+suminiee@users.noreply.github.com>
2022-06-03 23:10:31 +0900

    Delete 8 directory

--- 8/새 폴더/6회차 (BSTree)/BSTree.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Tree.h"
-
-typedef int (*CompareElement)(Element* e1, Element* e2);
-
-PTreeNode BSTreInsert(PTreeNode root, Element e, CompareElement compare);
-PTreeNode BSTreDelete(PTreeNode root, Element e, CompareElement compare);
-PTreeNode BSTreSearch(PTreeNode root, Element e, CompareElement compare);
--- 8/새 폴더/6회차 (BSTree)/DAType.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-// DynamicArray에 저장될 배열 요소의 자료형
-typedef int Element;
